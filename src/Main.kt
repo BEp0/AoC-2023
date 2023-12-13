9b108d17e69d05a62415341cf6e7027c74fc0787
Bernardo Porto e Silva Souza <bernardo.souza@cwi.com.br>
2023-12-13 15:39:14 -0300

    day 4 part 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,11 @@
 import days.Day01
 import days.Day02
 import days.Day03
+import days.Day04
 
 fun main() {
 
-    val days = listOf(Day01(), Day02(), Day03())
+    val days = listOf(Day01(), Day02(), Day03(), Day04())
     days.forEach { day -> day.exec() }
 
 }
