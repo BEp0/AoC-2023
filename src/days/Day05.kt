3593ac26cf37f8c86db5b0434f4875191546586e
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2023-12-14 10:42:22 -0300

    input size

--- src/days/Day05.kt
+++ src/days/Day05.kt
@@ -4,17 +4,17 @@ import readInput
 
 class Day05(
     override val day: Int = 5,
-    override val part1ExpectationTest: Int = 0,
-    override val part2ExpectationTest: Int = 0,
+    override val part1ExpectationTest: Int = 33,
+    override val part2ExpectationTest: Int = 33,
     override val inputPart: List<String> = readInput("day05"),
     override val inputTest1: List<String> = readInput("day05_test"),
     override val inputTest2: List<String> = readInput("day05_test"),
 ) : Day<Int, List<String>> {
     override fun part1(input: List<String>): Int {
-        TODO("Not yet implemented")
+        return input.size
     }
 
     override fun part2(input: List<String>): Int {
-        TODO("Not yet implemented")
+        return input.size
     }
 }
